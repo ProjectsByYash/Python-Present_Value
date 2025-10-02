a9d424b4f4e95a4bc7bb843f0d744b41071403db
ProjectsByYash <yashokmr1233@gamil.com>
2025-10-02 18:30:37 +0530

    Initial commit: add main.py, added the valueError

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import warnings
 
 
 def future_value_annuity(payment, rate, periods):
@@ -11,6 +11,14 @@ def future_value_annuity(payment, rate, periods):
     :return: Future Value
 
     """
+
+    if payment <=0:
+        warnings.warn("Payment should be positive. Did you mean to use a negative cash flow?")
+    if periods <=0:
+        raise ValueError("Number of periods must be greater than 0.")
+    if rate == 0:
+        return payment * periods #no interest case
+
     return payment * (((1+r)**periods-1)/rate)
 
 
@@ -25,12 +33,20 @@ def present_value_annuity(payment, rate, periods):
     :return: Present Value
     """
 
+    if payment <=0:
+        warnings.warn("Payment should be positive. Did you mean to use a negative cash flow?")
+    if periods <=0:
+        raise ValueError("Number of periods must be greater than 0.")
+    if rate == 0:
+        return payment * periods #no discounting case
+
+
     return payment * ((1-(1+rate)**-periods)/rate)
 
 
 
 pmt = 1000
-r= 0.05
+r= 0.0
 n=10
 
 fv = future_value_annuity(pmt,r,n)
